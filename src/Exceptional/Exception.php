9140e01c6a09f6bb7f72a2663dca731054a22810
Tom Wright <tom@inflatablecookie.com>
2020-09-29 18:08:58 +0000

    Added initial exception factory structure
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Exceptional/Exception.php
+++ src/Exceptional/Exception.php
@@ -6,6 +6,17 @@
 declare(strict_types=1);
 namespace DecodeLabs\Exceptional;
 
+use DecodeLabs\Glitch\Stack\Frame;
+use DecodeLabs\Glitch\Stack\Trace;
+
 interface Exception
 {
+    public function setData($data);
+    public function getData();
+
+    public function setHttpCode(?int $code);
+    public function getHttpCode(): ?int;
+
+    public function getStackFrame(): Frame;
+    public function getStackTrace(): Trace;
 }
---
+++ src/Exceptional/Factory.php
@@ -0,0 +1,674 @@
+<?php
+/**
+ * This file is part of the Exceptional package
+ * @license http://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+namespace DecodeLabs\Exceptional;
+
+use DecodeLabs\Exceptional\Exception;
+
+use Throwable;
+use InvalidArgumentException;
+use LogicException;
+
+/**
+ * Automatically generate Exceptions on the fly based on scope and
+ * requested interface types
+ */
+class Factory
+{
+    const STANDARD = [
+        'Logic' => [
+            'type' => 'LogicException'
+        ],
+            'BadFunctionCall' => [
+                'extend' => 'Logic',
+                'type' => 'BadFunctionCallException'
+            ],
+                'BadMethodCall' => [
+                    'extend' => 'BadFunctionCall',
+                    'type' => 'BadMethodCallException'
+                ],
+
+            'Domain' => [
+                'extend' => 'Logic',
+                'type' => 'DomainException'
+            ],
+            'InvalidArgument' => [
+                'extend' => 'Logic',
+                'type' => 'InvalidArgumentException'
+            ],
+            'Length' => [
+                'extend' => 'Logic',
+                'type' => 'LengthException'
+            ],
+            'OutOfRange' => [
+                'extend' => 'Logic',
+                'type' => 'OutOfRangeException'
+            ],
+
+            'Definition' => [
+                'extend' => 'Logic'
+            ],
+            'Implementation' => [
+                'extend' => 'Logic'
+            ],
+                'NotImplemented' => [
+                    'extend' => 'Implementation',
+                    'http' => 501
+                ],
+
+            'Unsupported' => [
+                'extend' => 'Logic'
+            ],
+
+
+        'Runtime' => [
+            'type' => 'RuntimeException'
+        ],
+            'OutOfBounds' => [
+                'extend' => 'Runtime',
+                'type' => 'OutOfBoundsException'
+            ],
+            'Overflow' => [
+                'extend' => 'Runtime',
+                'type' => 'OverflowException'
+            ],
+            'Range' => [
+                'extend' => 'Runtime',
+                'type' => 'RangeException'
+            ],
+            'Underflow' => [
+                'extend' => 'Runtime',
+                'type' => 'UnderflowException'
+            ],
+            'UnexpectedValue' => [
+                'extend' => 'Runtime',
+                'type' => 'UnexpectedValueException'
+            ],
+
+            'Io' => [
+                'extend' => 'Runtime'
+            ],
+                'Protocol' => [
+                    'extend' => 'Io'
+                ],
+
+            'BadRequest' => [
+                'extend' => 'Runtime',
+                'http' => 400
+            ],
+            'Unauthorized' => [
+                'extend' => 'Runtime',
+                'http' => 401
+            ],
+                'Forbidden' => [
+                    'extend' => 'Unauthorized',
+                    'http' => 403
+                ],
+            'NotFound' => [
+                'extend' => 'Runtime',
+            ],
+                'ResourceNotFound' => [
+                    'extend' => 'NotFound',
+                    'http' => 404
+                ],
+
+            'Setup' => [
+                'extend' => 'Runtime'
+            ],
+                'ComponentUnavailable' => [
+                    'extend' => 'Setup'
+                ],
+                'ServiceUnavailable' => [
+                    'extend' => 'Setup',
+                    'http' => 503
+                ],
+
+        'SystemError' => [
+            'type' => 'ErrorException'
+        ]
+    ];
+
+
+    const REWIND = 2;
+
+    private static $instances = [];
+
+    protected $message;
+    protected $params = [];
+
+    protected $baseClass;
+    protected $namespace;
+    protected $interfaces = [];
+    protected $traits = [];
+
+    protected $interfaceIndex = [];
+    protected $interfaceDefs = [];
+    protected $exceptionDef;
+
+
+    /**
+     * Generate a context specific, message oriented throwable error
+     */
+    public static function create(
+        array $types,
+        ?int $rewind=0,
+        ?string $message=null,
+        ?array $params=[],
+        $data=null,
+        ?Throwable $previous=null,
+        ?int $code=null,
+        ?string $file=null,
+        ?int $line=null,
+        ?int $http=null,
+        ?string $namespace=null,
+        ?array $interfaces=null,
+        ?array $traits=null
+    ): Exception {
+        return (new self(
+            $types, $rewind,
+            $message, $params, $data,
+            $previous, $code, $file, $line,
+            $http,
+            $namespace, $interfaces, $traits
+        ))->build();
+    }
+
+    /**
+     * Begin new factory process
+     */
+    protected function __construct(
+        array $types,
+        ?int $rewind,
+        ?string $message,
+        ?array $params,
+        $data,
+        ?Throwable $previous,
+        ?int $code,
+        ?string $file,
+        ?int $line,
+        ?int $http,
+        ?string $namespace,
+        ?array $interfaces,
+        ?array $traits
+    ) {
+        // Message
+        $this->message = $message ?? $params['message'] ?? 'Undefined error';
+
+
+        // Params
+        $this->params = $params ?? [];
+        $this->params['data'] = $data ?? $params['data'] ?? null;
+        $this->params['previous'] = $previous ?? $params['previous'] ?? null;
+        $this->params['code'] = (int)($code ?? $params['code'] ?? 0);
+
+        if (!$this->params['previous'] instanceof Throwable) {
+            $this->params['previous'] = null;
+        }
+
+        // Trace
+        $this->params['rewind'] = $rewind = (int)max((int)($rewind ?? $params['rewind'] ?? 1), 0);
+
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, (int)($rewind + static::REWIND + 1));
+        $key = $rewind + static::REWIND;
+        $lastTrace = $trace[$key - 1];
+
+        $this->params['file'] = $file ?? $params['file'] ?? $lastTrace['file'] ?? null;
+        $this->params['line'] = $line ?? $params['line'] ?? $lastTrace['line'] ?? null;
+
+        if (!is_string($this->params['file'])) {
+            $this->params['file'] = null;
+        }
+        if (!is_int($this->params['line'])) {
+            $this->params['line'] = null;
+        }
+
+        // Namespace
+        $this->prepareTargetNamespace(
+            $namespace ?? $params['namespace'] ?? null,
+            $trace[$key] ?? null
+        );
+
+        // Inheritance
+        $this->interfaceIndex['\\DecodeLabs\\Exceptional\\Exception'] = [];
+        $this->interfaceIndex['\\DecodeLabs\\Glitch\\Dumpable'] = [];
+        $this->traits['\\DecodeLabs\\Exceptional\\ExceptionTrait'] = true;
+
+        $this->importTypes($types);
+        $this->importInterfaces($interfaces ?? []);
+        $this->importInterfaces($this->params['interfaces'] ?? []);
+        $this->importTraits($traits ?? []);
+        $this->importTraits($this->params['traits'] ?? []);
+
+
+        // Cleanup
+        unset(
+            $this->params['message'],
+            $this->params['namespace'],
+            $this->params['interfaces'],
+            $this->params['traits']
+        );
+    }
+
+
+    /**
+     * Prepare target namespace
+     */
+    protected function prepareTargetNamespace(?string $namespace, ?array $frame): void
+    {
+        $this->namespace = $namespace;
+
+        if (
+            $this->namespace === null &&
+            $frame !== null
+        ) {
+            $class = $frame['class'] ?? null;
+
+            if (!empty($class)) {
+                if (false !== strpos($class, 'class@anon')) {
+                    $this->namespace = null;
+                } else {
+                    $parts = explode('\\', $class);
+                    array_pop($parts);
+                    $this->namespace = implode('\\', $parts);
+                }
+            }
+        }
+
+        if ($this->namespace !== null) {
+            $this->namespace = ltrim($this->namespace, '\\');
+        }
+
+        if (empty($this->namespace)) {
+            $this->namespace = null;
+        } else {
+            $this->namespace = '\\'.$this->namespace;
+        }
+    }
+
+
+    /**
+     * Import type definitions
+     */
+    protected function importTypes(array $types): void
+    {
+        foreach ($types as $type) {
+            $type = trim($type);
+
+            if (empty($type)) {
+                continue;
+            }
+
+            if (!preg_match('/(.+)(Exception)(Trait)?$/', $type, $matches)) {
+                $type .= 'Exception';
+            }
+
+            $isTraitName = isset($matches[3]);
+
+            if (false !== strpos($type, '/')) {
+                // Path style
+                $origType = $type;
+                $type = str_replace('/', '\\', $type);
+
+                if (substr($type, 0, 1) == '.') {
+                    if ($this->namespace === null) {
+                        throw new LogicException(
+                            'Stack context is not within a namespace for path dereferencing: '.$origType
+                        );
+                    }
+
+                    $type = $this->namespace.'\\'.$type;
+                }
+
+                $type = '\\'.ltrim($type, '\\');
+
+                if (false !== strpos($type, '.')) {
+                    $parts = [];
+
+                    foreach (explode('\\', $type) as $part) {
+                        if ($part == '.') {
+                            continue;
+                        } elseif ($part == '..') {
+                            array_pop($parts);
+                        } else {
+                            $parts[] = $part;
+                        }
+                    }
+
+                    $type = implode('\\', $parts);
+                }
+            } else {
+                // Namespace style
+                if (substr($type, 0, 1) !== '\\') {
+                    $type = $this->namespace.'\\'.$type;
+                }
+            }
+
+
+            // Named trait
+            if ($isTraitName) {
+                if (!trait_exists($type)) {
+                    throw new InvalidArgumentException(
+                        'Trait not found: '.$type
+                    );
+                }
+
+                $this->traits[$type] = true;
+                continue;
+            }
+
+            // Named class
+            if (class_exists($type)) {
+                if ($this->baseClass !== null) {
+                    throw new InvalidArgumentException(
+                        'Exception has already defined base type: '.$this->baseClass
+                    );
+                }
+
+                $this->baseClass = trim($type, '\\');
+                continue;
+            }
+
+            $this->interfaces[$type] = true;
+        }
+    }
+
+
+    /**
+     * Import interface definitions
+     */
+    protected function importInterfaces(array $interfaces): void
+    {
+        foreach ($interfaces as $interface) {
+            if (!interface_exists($interface)) {
+                throw new InvalidArgumentException(
+                    $interface.' is not an interface'
+                );
+            }
+
+            $this->interfaces[$interface] = true;
+        }
+    }
+
+    /**
+     * Import trait definitions
+     */
+    protected function importTraits(array $traits): void
+    {
+        foreach ($traits as $trait) {
+            if (!trait_exists($trait)) {
+                throw new InvalidArgumentException(
+                    $trait.' is not an trait'
+                );
+            }
+
+            $this->traits[$trait] = true;
+        }
+    }
+
+
+
+
+
+    /**
+     * Build exception
+     */
+    public function build(): Exception
+    {
+        // Named interfaces
+        foreach ($this->interfaces as $interface => $enabled) {
+            $this->indexInterface($interface);
+        }
+
+        // Definitions
+        $interfaces = $this->buildDefinitions();
+        $hash = $this->compileDefinitions();
+
+        // Params
+        $params = $this->params;
+        $params['type'] = $this->baseClass;
+        $params['interfaces'] = $interfaces;
+
+        // Instantiate
+        return new self::$instances[$hash]($this->message, $params);
+    }
+
+    /**
+     * Add interface info to class extend list
+     */
+    protected function indexInterface(string $interface): void
+    {
+        $parts = explode('\\', ltrim($interface, '\\'));
+        $name = substr((string)array_pop($parts), 0, -9);
+
+        // Trait
+        $traitName = $interface.'Trait';
+
+        if (trait_exists($traitName)) {
+            $this->traits[$traitName] = true;
+        }
+
+        // Namespace
+        if (!empty($parts)) {
+            $this->indexNamespaceInterfaces($parts);
+        }
+
+
+        // Interface
+        if ($classExists = class_exists($interface)) {
+            if ($this->baseClass !== null) {
+                throw new InvalidArgumentException(
+                    'Exception has already defined base type: '.$this->baseClass
+                );
+            }
+
+            $this->baseClass = trim($interface, '\\');
+        }
+
+
+        // Package
+        if (isset(static::STANDARD[$name])) {
+            $this->indexPackageInterface($name);
+
+            // Interface
+            if (!isset($this->interfaceIndex[$interface])) {
+                $this->interfaceIndex[$interface] = [
+                    '\\DecodeLabs\\Exceptional\\'.$name.'Exception'
+                ];
+
+                if (!empty($parts)) {
+                    $this->interfaceIndex[$interface][] = '\\'.implode('\\', $parts).'\\Exception';
+                }
+            }
+        } else {
+            // User
+            if (!isset($this->interfaceIndex[$interface])) {
+                $this->interfaceIndex[$interface] = [
+                    '\\DecodeLabs\\Exceptional\\Exception'
+                ];
+            }
+        }
+    }
+
+    /**
+     * Index namespace interface
+     */
+    protected function indexNamespaceInterfaces(array $parts): void
+    {
+        $set = [];
+        $last = '\\DecodeLabs\\Exceptional\\Exception';
+
+        while (!empty($parts)) {
+            $set[] = array_shift($parts);
+            $interface = '\\'.implode('\\', $set).'\\Exception';
+
+            // Check
+            if (class_exists($interface)) {
+                throw new LogicException(
+                    'Requested interface exists as a class: '.$interface
+                );
+            }
+
+            if (
+                isset($this->interfaceIndex[$interface]) ||
+                interface_exists($interface)
+            ) {
+                continue;
+            }
+
+            $this->interfaceIndex[$interface] = [$last];
+            $last = $interface;
+        }
+    }
+
+
+    /**
+     * Index package interface
+     */
+    protected function indexPackageInterface(string $name): void
+    {
+        $standard = static::STANDARD[$name];
+        $prefix = '\\DecodeLabs\\Exceptional\\';
+        $interface = $prefix.$name.'Exception';
+
+        // Check
+        if (class_exists($interface)) {
+            throw new LogicException(
+                'Requested interface exists as a class: '.$interface
+            );
+        }
+
+        if (
+            isset($this->interfaceIndex[$interface]) ||
+            interface_exists($interface)
+        ) {
+            return;
+        }
+
+        // Base class
+        if (
+            isset($standard['type']) &&
+            $this->baseClass === null
+        ) {
+            $this->baseClass = $standard['type'];
+        }
+
+        // Http
+        if (
+            isset($standard['http']) &&
+            !isset($this->params['http'])
+        ) {
+            $this->params['http'] = $standard['http'];
+        }
+
+
+        // Interface
+        if (isset($standard['extend'])) {
+            $this->indexPackageInterface($standard['extend']);
+            $this->interfaceIndex[$interface] = [$prefix.$standard['extend'].'Exception'];
+        } else {
+            $this->interfaceIndex[$interface] = [$prefix.'Exception'];
+        }
+    }
+
+
+
+
+
+
+    /**
+     * Build interface definitions
+     */
+    protected function buildDefinitions(): array
+    {
+        // Ensure base class
+        if ($this->baseClass === null) {
+            $this->baseClass = \Exception::class;
+        }
+
+        // Create definitions for needed interfaces
+        foreach ($this->interfaceIndex as $interface => $extends) {
+            if (!empty($extends)) {
+                $this->defineInterface($interface, $extends);
+            }
+        }
+
+
+        // Build class def
+        $this->exceptionDef = 'return new class(\'\') extends '.$this->baseClass;
+        $interfaces = $this->interfaceIndex;
+
+        foreach ($this->interfaceIndex as $interface => $extends) {
+            foreach ($extends as $extend) {
+                unset($interfaces[$extend]);
+            }
+        }
+
+        if (empty($interfaces)) {
+            $interfaces[] = Exception::class;
+        } else {
+            $interfaces = array_keys($interfaces);
+        }
+
+        $this->exceptionDef .= ' implements '.implode(',', $interfaces);
+        $this->exceptionDef .= ' {';
+
+        foreach ($this->traits as $trait => $enabled) {
+            $this->exceptionDef .= 'use '.$trait.';';
+        }
+
+        $this->exceptionDef .= '};';
+
+        // Output interfaces
+        foreach ($interfaces as $i => $interface) {
+            if (empty($this->interfaceIndex[$interface])) {
+                unset($interfaces[$i]);
+            }
+        }
+
+        return $interfaces;
+    }
+
+    /**
+     * Define interface
+     */
+    protected function defineInterface(string $interface, array $extends): void
+    {
+        if (interface_exists($interface)) {
+            return;
+        }
+
+        $parts = explode('\\', ltrim($interface, '\\'));
+        $name = array_pop($parts);
+        $namespace = implode('\\', $parts);
+        $parents = implode(',', $extends);
+
+        $this->interfaceDefs[$interface] = 'namespace '.$namespace.' { interface '.$name.' extends '.$parents.' {} }';
+    }
+
+
+    /**
+     * Compile definitions using eval()
+     */
+    protected function compileDefinitions(): string
+    {
+        $defs = implode("\n", $this->interfaceDefs);
+
+        // Put the eval code in $GLOBALS to dump if it dies
+        $GLOBALS['__eval'] = $defs."\n".$this->exceptionDef;
+
+        eval($defs);
+        $hash = md5($this->exceptionDef);
+
+        if (!isset(self::$instances[$hash])) {
+            self::$instances[$hash] = eval($this->exceptionDef);
+        }
+
+        // Remove defs from $GLOBALS again
+        unset($GLOBALS['__eval']);
+
+        return $hash;
+    }
+}
