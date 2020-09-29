a12df0ee507c47e78fee62047241566a413e4d2b
Tom Wright <tom@inflatablecookie.com>
2020-09-29 12:30:09 +0000

    Added Exception interface
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Exceptional/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the Exceptional package
+ * @license http://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+namespace DecodeLabs\Exceptional;
+
+interface Exception
+{
+}
