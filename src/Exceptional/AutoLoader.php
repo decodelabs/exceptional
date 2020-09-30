dba3f2a489a22e21dc4d5e5c000618ade6b0dc50
Tom Wright <tom@inflatablecookie.com>
2020-09-30 16:25:04 +0000

    Added initial Exception autoloader
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Exceptional/AutoLoader.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the Exceptional package
+ * @license http://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+namespace DecodeLabs\Exceptional;
+
+use DecodeLabs\Exceptional\Factory;
+
+class AutoLoader
+{
+    protected static $registered = false;
+
+    /**
+     * Is registered as autoLoader
+     */
+    public static function isRegistered(): bool
+    {
+        return self::$registered;
+    }
+
+    /**
+     * Register as autoloader
+     */
+    public static function register(): void
+    {
+        if (!self::$registered) {
+            \spl_autoload_register([self::class, 'loadClass']);
+            self::$registered = true;
+        }
+    }
+
+    /**
+     * Unregister as autoloader
+     */
+    public static function unregister(): void
+    {
+        if (self::$registered) {
+            \spl_autoload_unregister([self::class, 'loadClass']);
+            self::$registered = false;
+        }
+    }
+
+    public static function loadClass(string $class): void
+    {
+        if (!preg_match('/\\\\([a-zA-Z0-9_]?)Exception$/', $class)) {
+            return;
+        }
+
+        Factory::create([$class], 1, 'AutoLoader');
+    }
+}
