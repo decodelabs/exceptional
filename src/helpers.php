7114a020ad8df454de2ff81652ac9c538ce9fbcc
Tom Wright <tom@inflatablecookie.com>
2020-10-06 15:23:37 +0000

    Updated helpers header
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @package Exceptional
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
 namespace DecodeLabs;
 
 Exceptional\AutoLoader::register();
